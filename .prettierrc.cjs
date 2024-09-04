250841c994131540b16558d9b33efae510e10359
tsumo <tsumo@users.noreply.github.com>
2024-09-04 14:21:09 +0300

    chore: delete template code, expand configs

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: 'typescript',
+    arrowParens: 'always',
+    printWidth: 120,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    endOfLine: 'auto',
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'all',
+};
+
