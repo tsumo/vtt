2188448b894ab6804daa2a1f7ca2a9399cb46f58
tsumo <tsumo@users.noreply.github.com>
2024-09-24 00:43:25 +0300

    feat: pass base url to config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  base: process.env.BASE_URL,
 })
