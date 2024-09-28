522879fa8b81a642ede2f36a87eff0bdc71a0c59
tsumo <tsumo@users.noreply.github.com>
2024-09-28 12:33:52 +0300

    feat: move line geo construction to function

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -10,48 +10,51 @@ const {
 
 const halfLineWidth = lineWidth / 2
 
-const points: V2[] = [
+const constructLineGeometry = (points: V2[]): THREE.BufferGeometry => {
+  const vertices: number[] = []
+  for (let i = 0; i < points.length - 1; i++) {
+    const x1 = points[i][0] * gridStep
+    const y1 = points[i][1] * gridStep
+    const x2 = points[i + 1][0] * gridStep
+    const y2 = points[i + 1][1] * gridStep
+    const angle = Math.atan2(y2 - y1, x2 - x1)
+
+    // All lines start at this position and then get rotated
+    //    p1 +------------------+ p2
+    //       |                  |
+    // x1,y1 +                  + x2,y2
+    //       |                  |
+    //    p4 +------------------+ p3
+
+    const upperPointsAngle = angle - rads[90]
+    const lowerPointsAngle = angle + rads[90]
+    const p1 = [...getCirclePoint(upperPointsAngle, halfLineWidth, x1, y1), 0]
+    const p2 = [...getCirclePoint(upperPointsAngle, halfLineWidth, x2, y2), 0]
+    const p3 = [...getCirclePoint(lowerPointsAngle, halfLineWidth, x2, y2), 0]
+    const p4 = [...getCirclePoint(lowerPointsAngle, halfLineWidth, x1, y1), 0]
+
+    vertices.push(...p1, ...p2, ...p3)
+    vertices.push(...p1, ...p3, ...p4)
+  }
+
+  const geometry = new THREE.BufferGeometry()
+  geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3))
+  return geometry
+}
+
+const geometry = constructLineGeometry([
   [0, 0],
   [1, 1],
   [1, 2],
   [0, 1],
   [-1, 1],
   [-1, 3],
-]
-
-const vertices: number[] = []
-for (let i = 0; i < points.length - 1; i++) {
-  const x1 = points[i][0] * gridStep
-  const y1 = points[i][1] * gridStep
-  const x2 = points[i + 1][0] * gridStep
-  const y2 = points[i + 1][1] * gridStep
-  const angle = Math.atan2(y2 - y1, x2 - x1)
-
-  // All lines start at this position and then get rotated
-  //    p1 +--------------+ p2
-  //       |              |
-  // x1,y1 +              + x2,y2
-  //       |              |
-  //    p4 +--------------+ p3
-
-  const upperPointsAngle = angle - rads[90]
-  const lowerPointsAngle = angle + rads[90]
-  const p1 = [...getCirclePoint(upperPointsAngle, halfLineWidth, x1, y1), 0]
-  const p2 = [...getCirclePoint(upperPointsAngle, halfLineWidth, x2, y2), 0]
-  const p3 = [...getCirclePoint(lowerPointsAngle, halfLineWidth, x2, y2), 0]
-  const p4 = [...getCirclePoint(lowerPointsAngle, halfLineWidth, x1, y1), 0]
-
-  vertices.push(...p1, ...p2, ...p3)
-  vertices.push(...p1, ...p3, ...p4)
-}
-
-const geometry = new THREE.BufferGeometry()
-geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3))
+])
 
 const material = new THREE.MeshBasicMaterial({
   color: config.colors.line,
   transparent: true,
-  opacity: 0.5,
+  // opacity: 0.5,
   // side: THREE.DoubleSide,
   wireframe: true,
 })
